d425dac27701cba92acf1249d7d6471bd1fa11aa
6434657 <d.s.vandam@students.uu.nl>
2025-05-14 12:46:29 +0200

    added prelim code

--- assignment 2/code/read_data.py
+++ assignment 2/code/read_data.py
@@ -1,55 +1,24 @@
-{
- "cells": [
-  {
-   "cell_type": "markdown",
-   "metadata": {},
-   "source": [
-    "# Reading of the data\n",
-    "The files contained in each of those folders have the “h5” extension. In order\n",
-    "to read them, you need to use the h5py library ( that you can install using\n",
-    "“pip install h5py” if you don’t have it already ). This type of files can contain\n",
-    "datasets identified by a name. For simplicity, each file contains only 1 dataset.\n",
-    "The following code snippet can read the file ”Intra/train/rest 105923 1.h5”:"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import h5py\n",
-    "import os\n",
-    "\n",
-    "# TODO: code that will obtain the shared directory will collectively using OS libary\n",
-    "shared_directory = \"\"\n",
-    "\n",
-    "def get_dataset_name(file_name_with_dir):\n",
-    "    filename_without_dir = file_name_with_dir.split('/')[-1]\n",
-    "    temp = filename_without_dir.split('.')[:-1]\n",
-    "    dataset_name = \"_\".join(temp)\n",
-    "    return dataset_name\n",
-    "\n",
-    "filename_path = \"Intra/train/rest_105923_1.h5\"\n",
-    "\n",
-    "# Reads in data based on some file name path as above\n",
-    "with h5py.File(filename_path, 'r') as f:\n",
-    "    dataset_name = get_dataset_name(filename_path)\n",
-    "    matrix = f.get(dataset_name)[()]\n"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "name": "python",
-   "version": "3.12.3"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+import h5py
+import os
+import numpy as np
+
+# TODO: use os to do relative path from the working directory
+
+source_dir: str = "extracted_zip_in_here"
+
+def get_dataset_name(file_name_with_dir, taskType) -> str:
+    # TODO if no subject identifier is supplied return selection message
+    # or option to obtain all (with warning)
+
+    # TODO: obtain all files in source data directory
+    filename_without_dir = file_name_with_dir.split('/')[-1]
+    temp = filename_without_dir.split('.')[:-1]
+    dataset_name = "_".join(temp)
+    return dataset_name
+
+# TODO: Function that given a path reads in the data and return numpy array
+def read_data(file_name_with_dir) -> np.ndarray:
+    dataset_name = 
+    with h5py.File(file_name_with_dir, 'r') as f:
+        dataset_name = list(f.keys())[0]
+        data = f[dataset_name][:]
